9db99ba9ee1a156529ddce69816b4bf0340340bd
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2025-05-03 14:33:03 -0500

    Add files via upload

---
+++ basic-javascript/script/myScript.js
@@ -0,0 +1,4 @@
+document.getElementById("changeTextBtn").addEventListener("click", function() {
+    document.getElementById("introText").textContent = "The text has been changed!";
+    document.getElementById("contentText").textContent = "This section has also been updated!";
+});
